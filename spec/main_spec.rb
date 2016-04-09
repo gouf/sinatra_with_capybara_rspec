88b12239dab13e7cf8e990d02b5ad42df12e872a
Gou Furuya <innocent.zero@gmail.com>
2016-04-10 00:14:37 +0900

    Save screenshot test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,11 +1,16 @@
 Capybara.app = MyApp
 # Capybara.default_driver = :selenium
+Capybara.default_driver = :poltergeist
 Capybara.app_host = 'http://localhost:3000'
 
 describe 'Show Top Page' do
   it 'test' do
     visit 'index.html'
+    file_path = File.expand_path('./screenshot.jpg')
+    script = %(document.body.style.backgroundColor = "white")
+    page.execute_script(script)
+    save_screenshot(file_path, full: true)
     expect(page).to have_content 'click'
-    expect(page).to have_field 'input'
+    expect(page.find(:css, 'form > input')).to be_truthy
   end
 end
