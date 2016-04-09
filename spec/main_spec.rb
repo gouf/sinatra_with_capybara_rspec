55c8284053c1d9658ccd19ef75fd38ad752d6171
Gou Furuya <innocent.zero@gmail.com>
2016-04-09 22:48:34 +0900

    Remove unused code

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,7 +2,7 @@ Capybara.app = MyApp
 # Capybara.default_driver = :selenium
 Capybara.app_host = 'http://localhost:3000'
 
-describe 'Show Top Page', type: :feature do
+describe 'Show Top Page' do
   it 'test' do
     visit 'index.html'
     expect(page).to have_content 'click'
